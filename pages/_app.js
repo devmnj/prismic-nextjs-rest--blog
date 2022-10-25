d905f41d19347108869949a4f4040a1b13deba9d
manojap <manojap@outlook.com>
2022-10-25 22:56:55 +0530

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,16 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
-import Link from 'next/link'
-import { PrismicPreview } from '@prismicio/next'
-import { PrismicProvider } from '@prismicio/react'
-import { repositoryName ,client} from '.././prismicio'
+ 
 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PrismicProvider
-       client={client}
-      
-    >
-  <PrismicPreview repositoryName={repositoryName}>
+    
+   
   <Layout>
     <Component {...pageProps} />
-  </Layout></PrismicPreview>
-  </PrismicProvider>
+  </Layout> 
+  
   )
 }
 
